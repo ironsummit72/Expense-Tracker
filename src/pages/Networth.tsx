fff05b78d186f407752f2125f236825c8c4c793a
ironsummit72 <ironsumit72@gmail.com>
2024-11-26 23:45:12 +0530

    page is under development

--- src/pages/Networth.tsx
+++ src/pages/Networth.tsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom"
+
 
 function Networth() {
   return (
-    <div>Networth</div>
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="flex flex-col items-center gap-5">
+      <h1 className="text-2xl">Page is Under Development {":("}</h1>
+      <Link className="text-blue-600 dark:text-blue-500 hover:underline" to='/'>Go Back</Link>
+      </div>
+    </div>
   )
 }
 
